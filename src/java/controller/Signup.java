e3141e01d87961a0803dff77bfd7b2e3c4098fc2
iamteerawut <t.kitbunjerdjarud@gmail.com>
2017-04-12 16:56:42 +0700

    Update

--- src/java/controller/Signup.java
+++ src/java/controller/Signup.java
@@ -6,10 +6,9 @@
 package controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -22,7 +21,7 @@ import model.Profile;
  *
  * @author tkitb
  */
-@WebServlet(name = "signup", urlPatterns = {"/signup"})
+@WebServlet(name = "Signup", urlPatterns = {"/Signup"})
 public class Signup extends HttpServlet {
 
     /**
@@ -35,29 +34,27 @@ public class Signup extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException, SQLException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-////        String username = request.getParameter("username");
-////        String first_name = request.getParameter("first_name");
-////        String last_name = request.getParameter("last_name");
-////        String password = request.getParameter("password");
-////        String email = request.getParameter("email");
-////        
-////        ServletContext context = getServletContext();
-////        Connection connection = (Connection) context.getAttribute("connection");
-////        
-////        try {
-////            Profile profile = new Profile();
-////            profile.setUsername(username);
-////            profile.setFirst_name(first_name);
-////            profile.setLast_name(last_name);
-////            profile.addNewProfile(connection);
-////        } catch (SQLException ex) {
-////            ex.printStackTrace();
-////        }
-//        ของธี
+        String username = request.getParameter("username");
+        String first_name = request.getParameter("first_name");
+        String last_name = request.getParameter("last_name");
+        String password = request.getParameter("password");
+        String email = request.getParameter("email");
 
-//        String username = request.getParameter("username");
+        ServletContext context = getServletContext();
+        Connection connection = (Connection) context.getAttribute("connection");
+
+        try {
+            Profile profile = new Profile();
+            profile.setUsername(username);
+            profile.setFirst_name(first_name);
+            profile.setLast_name(last_name);
+            profile.addNewProfile(connection);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        //        String username = request.getParameter("username");
 //        String password = request.getParameter("password");
 //        String f_name = request.getParameter("f_name");
 //        String l_name = request.getParameter("l_name");
@@ -81,47 +78,45 @@ public class Signup extends HttpServlet {
 //            }
 //        }
 //    }
-        // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-        /**
-         * Handles the HTTP <code>GET</code> method.
-         *
-         * @param request servlet request
-         * @param response servlet response
-         * @throws ServletException if a servlet-specific error occurs
-         * @throws IOException if an I/O error occurs
-         */
-        @Override
-        protected void doGet
-        (HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-            processRequest(request, response);
-        }
-
-        /**
-         * Handles the HTTP <code>POST</code> method.
-         *
-         * @param request servlet request
-         * @param response servlet response
-         * @throws ServletException if a servlet-specific error occurs
-         * @throws IOException if an I/O error occurs
-         */
-        @Override
-        protected void doPost
-        (HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-            processRequest(request, response);
-        }
-
-        /**
-         * Returns a short description of the servlet.
-         *
-         * @return a String containing servlet description
-         */
-        @Override
-        public String getServletInfo
-        
-            () {
-        return "Short description";
-        }// </editor-fold>
-
     }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
