f3aae63326cc4b3c4736967082eb19bd3b30ba91
iamteerawut <t.kitbunjerdjarud@gmail.com>
2017-04-30 22:37:50 +0700

    Update authen

--- web/asset/js/authen.js
+++ web/asset/js/authen.js
@@ -1,6 +1,17 @@
 var firstnameCheck = false;
 var lastnameCheck = false;
 var usernameCheck = false;
+var emailCheck = false;
+var phoneCheck = false;
+
+var password = "1";
+var confirmPassword = "2";
+
+var passIn = document.getElementById("passIn");
+var passInDiv = document.getElementById("passInDiv");
+
+var confirmPassIn = document.getElementById("confirmPassIn");
+var confirmPassInDiv = document.getElementById("confirmPassInDiv");
 
 var signupButton = document.getElementById("signUpButton");
 
@@ -106,8 +117,143 @@ document.getElementById("usernameIn").addEventListener("input", function() {
     }
 });
 
+document.getElementById("emailIn").addEventListener("input", function() {
+    var emailIn = document.getElementById("emailIn");
+    var emailInDiv = document.getElementById("emailInDiv");
+    var email = emailIn.value;
+
+    if (email.length <= 2) {
+        emailInDiv.classList.remove("has-success");
+        emailInDiv.classList.remove("has-danger");
+        emailIn.classList.remove("form-control-success");
+        emailIn.classList.remove("form-control-danger");
+        emailCheck = false;
+        check();
+    } else if (email.length >= 10) {
+        emailInDiv.classList.add("has-success");
+        emailInDiv.classList.remove("has-danger");
+        emailIn.classList.add("form-control-success");
+        emailIn.classList.remove("form-control-danger");
+        emailCheck = true;
+        check();
+    } else {
+        emailInDiv.classList.remove("has-success");
+        emailInDiv.classList.add("has-danger");
+        emailIn.classList.remove("form-control-success");
+        emailIn.classList.add("form-control-danger");
+        emailCheck = false;
+        check();
+    }
+});
+
+document.getElementById("phoneIn").addEventListener("input", function() {
+    var phoneIn = document.getElementById("phoneIn");
+    var phoneInDiv = document.getElementById("phoneInDiv");
+    var phone = phoneIn.value;
+
+    if (phone.length <= 2) {
+        phoneInDiv.classList.remove("has-success");
+        phoneInDiv.classList.remove("has-danger");
+        phoneIn.classList.remove("form-control-success");
+        phoneIn.classList.remove("form-control-danger");
+        phoneCheck = false;
+        check();
+    } else if (phone.length == 10) {
+        phoneInDiv.classList.add("has-success");
+        phoneInDiv.classList.remove("has-danger");
+        phoneIn.classList.add("form-control-success");
+        phoneIn.classList.remove("form-control-danger");
+        phoneCheck = true;
+        check();
+    } else {
+        phoneInDiv.classList.remove("has-success");
+        phoneInDiv.classList.add("has-danger");
+        phoneIn.classList.remove("form-control-success");
+        phoneIn.classList.add("form-control-danger");
+        phoneCheck = false;
+        check();
+    }
+});
+
+document.getElementById("passIn").addEventListener("input", function() {
+    var passIn = document.getElementById("passIn");
+    var passInDiv = document.getElementById("passInDiv");
+    password = passIn.value;
+    confirmpassword = confirmPassIn.value;
+
+    if (password.length >= 8) {
+    	if (password === confirmpassword) {
+    		passInDiv.classList.add("has-success");
+        	passInDiv.classList.remove("has-danger");
+        	passIn.classList.add("form-control-success");
+        	passIn.classList.remove("form-control-danger");
+        	confirmPassInDiv.classList.add("has-success");
+        	confirmPassInDiv.classList.remove("has-danger");
+        	confirmPassIn.classList.add("form-control-success");
+        	confirmPassIn.classList.remove("form-control-danger");
+    	} else {
+    		passInDiv.classList.remove("has-success");
+        	passInDiv.classList.add("has-danger");
+        	passIn.classList.remove("form-control-success");
+        	passIn.classList.add("form-control-danger");
+        	confirmPassInDiv.classList.remove("has-success");
+        	confirmPassInDiv.classList.add("has-danger");
+        	confirmPassIn.classList.remove("form-control-success");
+        	confirmPassIn.classList.add("form-control-danger");
+    	}
+    } else {
+    		passInDiv.classList.remove("has-success");
+        	passInDiv.classList.remove("has-danger");
+        	passIn.classList.remove("form-control-success");
+        	passIn.classList.remove("form-control-danger");
+        	confirmPassInDiv.classList.remove("has-success");
+        	confirmPassInDiv.classList.remove("has-danger");
+        	confirmPassIn.classList.remove("form-control-success");
+        	confirmPassIn.classList.remove("form-control-danger");
+    	}
+});
+
+document.getElementById("confirmPassIn").addEventListener("input", function() {
+    var confirmPassIn = document.getElementById("confirmPassIn");
+    var confirmPassInDiv = document.getElementById("confirmPassInDiv");
+    password = passIn.value;
+    confirmpassword = confirmPassIn.value;
+
+    if (confirmpassword.length >= 8) {
+    	if (password === confirmpassword) {
+    		passInDiv.classList.add("has-success");
+        	passInDiv.classList.remove("has-danger");
+        	passIn.classList.add("form-control-success");
+        	passIn.classList.remove("form-control-danger");
+        	confirmPassInDiv.classList.add("has-success");
+        	confirmPassInDiv.classList.remove("has-danger");
+        	confirmPassIn.classList.add("form-control-success");
+        	confirmPassIn.classList.remove("form-control-danger");
+    	} else {
+    		passInDiv.classList.remove("has-success");
+        	passInDiv.classList.add("has-danger");
+        	passIn.classList.remove("form-control-success");
+        	passIn.classList.add("form-control-danger");
+        	confirmPassInDiv.classList.remove("has-success");
+        	confirmPassInDiv.classList.add("has-danger");
+        	confirmPassIn.classList.remove("form-control-success");
+        	confirmPassIn.classList.add("form-control-danger");
+    	}
+    } else {
+    		passInDiv.classList.remove("has-success");
+        	passInDiv.classList.remove("has-danger");
+        	passIn.classList.remove("form-control-success");
+        	passIn.classList.remove("form-control-danger");
+        	confirmPassInDiv.classList.remove("has-success");
+        	confirmPassInDiv.classList.remove("has-danger");
+        	confirmPassIn.classList.remove("form-control-success");
+        	confirmPassIn.classList.remove("form-control-danger");
+    }
+    check();
+});
+
 function check() {
-    if (firstnameCheck && lastnameCheck && usernameCheck) {
+    if (firstnameCheck && lastnameCheck && usernameCheck && emailCheck && phoneCheck && password === confirmpassword) {
         signupButton.removeAttribute("disabled");
     } else {
         signupButton.setAttribute("disabled", "disabled")
